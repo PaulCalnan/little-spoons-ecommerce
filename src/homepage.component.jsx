c96d800d0a1e7a406f9262d630e9f08ca64f1bc4
Paul Calnan <pcalnan71@gmail.com>
2022-01-15 06:53:19 +1100

    start homepage layout

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>TREAT CATEGORY 1</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>TREAT CATEGORY 2</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>TREAT CATEGORY 3</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>TREAT CATEGORY 4</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>TREAT CATEGORY 5</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+    
+);
+
+export default HomePage;
