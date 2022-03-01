97992ac23f35dd7c88b0103e71daa6e8e8fc3123
Paul Calnan <pcalnan71@gmail.com>
2022-03-02 05:02:53 +1100

    start implementing single item show

---
+++ src/components/item-detail/item-detail.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { CollectionItem } from '../collection-item/collection-item';
+
+import { CollectionItemContainer } from '../collection-item/collection-item.styles';
