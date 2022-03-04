9953a8f86e91926f2e9617f84e77e532e6f0be79
Paul Calnan <pcalnan71@gmail.com>
2022-03-05 07:49:09 +1100

    cleanup unused components

--- src/components/item-detail/item-detail.component.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import { CollectionItem } from '../collection-item/collection-item';
-
-import { CollectionItemContainer } from '../collection-item/collection-item.styles';
